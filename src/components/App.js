750d574f6ba1e7f3066161f18a8161cb35fa4d1c
Karoki <wanjikukar19@gmail.com>
2025-04-09 13:06:46 +0300

    Initial

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,21 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+  const user = {
+    name:"Liza",
+    city: "NewYork",
+    bio: "I made this!",
+    links: {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/"
+    }
+  };
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home  name = {user.name} city = {user.city} />
+      <About  bio = {user.bio} github = {user.github} linkedin = {user.linkedin} />
     </div>
   );
 }
